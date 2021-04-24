dd3ee76974fc3a05254ac2b9b1583965c537ef76
vncs2012 <vncs2012@gmail.com>
2021-04-24 00:28:14 -0300

    Setting  add hosts of heroku

--- RETHI/settings.py
+++ RETHI/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = 'c7citm%t5to$1deuef2fe#0)op&lba_6frupx_1yep=f-8(%9h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ALLOWED_HOSTS
+ALLOWED_HOSTS = ['https://eneagrama-app.herokuapp.com',
+                 'localhost', '127.0.0.1']
 ###############################################################################
 # Application definition
 
@@ -156,9 +156,9 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 JET_THEMES = [
     {
-        'theme': 'default', # theme folder name
-        'color': '#47bac1', # color of the theme's button in user menu
-        'title': 'Default' # theme title
+        'theme': 'default',  # theme folder name
+        'color': '#47bac1',  # color of the theme's button in user menu
+        'title': 'Default'  # theme title
     },
     {
         'theme': 'green',
