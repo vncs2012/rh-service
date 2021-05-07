582ac864e00e70f810c951996d113169a2ad71ee
vncs2012 <vncs2012@gmail.com>
2021-05-07 00:09:58 -0300

    template Validar Dados para Resposta do questionario

--- participantes/urls.py
+++ participantes/urls.py
@@ -3,7 +3,7 @@ from participantes import views as myapp_views
 
 urlpatterns = [
     path('questionario/paricipante', myapp_views.questionario_paricipante, name='questionario_paricipante'),
-    path('edit', myapp_views.paricipante_edit, name='paricipante_edit'),
+    path('validar-dados', myapp_views.paricipante_edit, name='paricipante_edit'),
     path('list', myapp_views.paricipante_list, name='paricipante_list'),
     path('create', myapp_views.create_participante, name='create_participante'),
     path('save', myapp_views.save, name='save_participante'),
