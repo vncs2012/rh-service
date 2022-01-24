ebf607b77448b8ae6cd1f4bc3a705a7dd0fe7d0d
vinicius.miranda <vncs2012@gmail.com>
2022-01-23 23:43:40 -0300

    heroku agora salus-br concerto link

--- RETHI/settings.py
+++ RETHI/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'c7citm%t5to$1deuef2fe#0)op&lba_6frupx_1yep=f-8(%9h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['eneagrama-app.herokuapp.com',
+ALLOWED_HOSTS = ['salus-br.herokuapp.com',
                  'localhost', '127.0.0.1']
 ###############################################################################
 # Application definition
